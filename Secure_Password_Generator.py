3120dee05f0146581e63c24486e969c454543524
Joseph DiCarlo <60683575+NearingIX@users.noreply.github.com>
2022-02-27 11:27:47 -0500

    Update Secure_Password_Generator.py

--- Secure_Password_Generator.py
+++ Secure_Password_Generator.py
@@ -4,25 +4,29 @@ import string
 
 print("How many passwords would you like to generate?")
 
-while True:
-    try:
-        numberOfPasswords = input()
-        passwordCount = int(numberOfPasswords)
-        for i in range(passwordCount):
-            print(
-                "".join(
-                    secrets.choice(
-                        string.punctuation
-                        + string.digits
-                        + string.ascii_lowercase
-                        + string.ascii_uppercase
+def generatePasswords():
+    while True:
+        try:
+            global i
+            numberOfPasswords = input()
+            passwordCount = int(numberOfPasswords)
+            for i in range(passwordCount):
+                print(
+                    "".join(
+                        secrets.choice(
+                            string.punctuation
+                            + string.digits
+                            + string.ascii_lowercase
+                            + string.ascii_uppercase
                         )
-                    # Password character length
-                    for i in range(14)
+                        # Password character length
+                        for i in range(14)
+                    )
                 )
-            )
-        print("Thank you!")
-        break
-    except ValueError:
-        print("You did not enter a number.")
-        continue
+            print("Thank you!")
+            break
+        except ValueError:
+            print("You did not enter a number.")
+            continue
+
+generatePasswords()
